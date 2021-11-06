b64cb7b019ce822fe505300a9f6495becd3cea5a
olga-cerovic <olga.cerovic@stud.noroff.no>
2021-11-06 12:54:07 +0100

    Js update
    Deleted comments (questions)

--- js/question-1.js
+++ js/question-1.js
@@ -1,11 +1,3 @@
-/*
-Question 1
-Convert the function below to an arrow function:
-function getRemainder(a,b) {
-return a % b;
-}
-*/
-
 const getRemainder = (a, b) => a % b;
 
 const result = getRemainder(10, 2);
